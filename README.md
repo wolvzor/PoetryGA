c87a92015eabe84510e3ec9292b8381792c3de29
wolvie <wolvie@wolviegames.com>
2016-03-24 21:08:20 -0600

    Added Stanford parser models. Updated tester for Stanford parser. NOTE: the new Stanford NLP dependencies are around 328mb - this takes a while to build.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # PoetryGA
 
+Please note that this project uses the Stanford NLP Parser plus Models. The models jar is approximately 320mb, so expect a long wait time when building for the first time.
+
+
 To run the application, execute:
 
 ./gradle wrapper
--- src/test/groovy/com/wolviegames/poetry/parser/ParserSpec.groovy
+++ src/test/groovy/com/wolviegames/poetry/parser/ParserSpec.groovy
@@ -1,6 +1,13 @@
 package com.wolviegames.poetry.parser
 
+import edu.stanford.nlp.ling.CoreLabel
+import edu.stanford.nlp.ling.HasWord
+import edu.stanford.nlp.ling.Sentence
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser
+import edu.stanford.nlp.parser.lexparser.Lexicon
+import edu.stanford.nlp.process.Tokenizer
+import edu.stanford.nlp.trees.Tree
+import edu.stanford.nlp.trees.TreebankLanguagePack
 import spock.lang.Specification
 
 
@@ -10,11 +17,23 @@ class ParserSpec extends Specification {
         setup:
         String parserModel = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
         LexicalizedParser lp = LexicalizedParser.loadModel(parserModel)
+        Lexicon lexicon = lp.getLexicon()
+        TreebankLanguagePack tlp = lp.getOp().langpack();
+
+        String originalString = ("This is a test string.");
 
         when:
+        Tokenizer<? extends HasWord> tokens =
+                tlp.getTokenizerFactory().getTokenizer(new StringReader(originalString));
+        List<? extends HasWord> newSentence = tokens.tokenize();
+
+        List<CoreLabel> rawWords = Sentence.toCoreLabelList(newSentence);
+        Tree parse = lp.apply(rawWords);
 
         then:
-
+        lexicon instanceof Lexicon
+        println(newSentence)
+        parse.pennPrint();
     }
 
 }
